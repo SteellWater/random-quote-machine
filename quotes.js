12d96be3512d021ac8c910021fac23bc82cdf9f2
SteellWater <132520474+SteellWater@users.noreply.github.com>
2023-11-12 10:48:28 +0000

    Updates

---
+++ quotes.js
@@ -0,0 +1,88 @@
+const quotes = [
+    {
+        quote: "I'm sick of following my dreams, man. I'm just going to ask where they're going and hook up with 'em later.",
+        author: "Mitch Hedberg"
+    },
+    {
+        quote: "Gentlemen, you can't fight in here. This is the war room",
+        author: "President Merkin Muffley, Dr. Strangelove"
+    },
+    {
+        quote: "My mother always used to say: The older you get, the better you get, unless you're a banana.",
+        author: "Rose (Betty White), The Golden Girls"
+    },
+    {
+        quote: "Halloween is the beginning of the holiday shopping season. That's for women. The beginning of the holiday shopping season for men is Christmas Eve.",
+        author: "David Letterman"
+    },
+    {
+        quote: "Before you criticize someone, you should walk a mile in their shoes. That way when you criticize them, you are a mile away from them and you have their shoes.",
+        author: "Jack Handey"
+    },
+    {
+        quote: "Before you marry a person, you should first make them use a computer with slow Internet to see who they really are.",
+        author: "Will Ferrell"
+    },
+    {
+        quote: "I love being married. It's so great to find that one special person you want to annoy for the rest of your life.",
+        author: "Rita Rudner"
+    },    
+    {
+        quote: "When your mother asks, 'Do you want a piece of advice?' it is a mere formality. It doesn't matter if you answer yes or no. You're going to get it anyway.",
+        author: "Erma Bombeck"
+    },
+    {
+        quote: "I want my children to have all the things I couldn't afford. Then I want to move in with them.",
+        author: "Phyllis Diller"
+    },
+    {
+        quote: "Insomnia sharpens your math skills because you spend all night calculating how much sleep you'll get if you're able to ‘fall asleep right now.'",
+        author: "Anonymous"
+    },
+    {
+        quote: "I used to sell furniture for a living. The trouble was, it was my own.",
+        author: "Les Dawson"
+    },
+    {
+        quote: "There is no sunrise so beautiful that it is worth waking me up to see it.",
+        author: "Mindy Kaling,Is Everyone Hanging Out Without Me?"
+    },
+    {
+        quote: "You know you've reached middle age when you're cautioned to slow down by your doctor, instead of by the police.",
+        author: "Joan Rivers"
+    },
+    {
+        quote: "Common sense is like deodorant. The people who need it most never use it.",
+        author: "Anonymous"
+    },
+    {
+        quote: "If I'm not back in five minutes, just wait longer.",
+        author: "Ace Ventura (Jim Carrey)"
+    },
+    {
+        quote: "As you get older, three things happen. The first is your memory goes, and I can't remember the other two.",
+        author: "Sir Norman Wisdom"
+    },
+    {
+        quote: "Accept who you are. Unless you're a serial killer.",
+        author: "Ellen DeGeneres"
+    },
+    {
+        quote: "I prefer not to think before speaking. I like being as surprised as everyone else by what comes out of my mouth.",
+        author: "Anonymous"
+    },
+    {
+        quote:"Never put off till tomorrow what you can do the day after tomorrow just as well.", 
+        author: "Mark Twain"
+    },
+    {
+        quote: "Good parenting means investing in your child's future, which is why I am saving to buy mine a hoverboard someday.",
+        author: "Lin-Manuel Miranda"
+    },
+    {
+        quote: "Trying is the first step toward failure.",
+        author: "Homer Simpson, The Simpsons"
+    }
+];
+
+export default quotes;
